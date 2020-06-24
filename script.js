d9468eaf5cf606cc46d8bdc011ab3dccef42c1e1
Mike <Mike>
2020-06-24 21:45:02 +0400

    day 2

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+const data = {
+  currentCardNumber: 1,
+  question2: null,
+  question3: [],
+  question4: null,
+  question5: {
+    name: "",
+    email: "",
+    confirm: false,
+  },
+};
+
+showCard(data.currentCardNumber);
+
+document.querySelector("[data-back]").addEventListener("click", (e) => {
+  data.currentCardNumber -= 1;
+  showCard(data.currentCardNumber);
+});
+const nextButton = document.querySelector("[data-next]");
+nextButton.addEventListener("click", (e) => {
+  data.currentCardNumber += 1;
+  showCard(data.currentCardNumber);
+});
+document.querySelector("[data-start]").addEventListener("click", (e) => {
+  data.currentCardNumber = 2;
+  showCard(data.currentCardNumber);
+});
+document.querySelector("[data-card='4']").addEventListener("click", (e) => {
+    let target = e.target.closest("li");
+    if (!target) {
+      return;
+    }
+    const inputElement = target.querySelector('input')
+    data.question4 = inputElement.value
+    showCard(data.currentCardNumber);
+  });
+  
+document.querySelector("[data-card='2']").addEventListener("click", (e) => {
+  let target = e.target.closest("li");
+  if (!target) {
+    return;
+  }
+  const inputElement = target.querySelector('input')
+  data.question2 = inputElement.value
+  showCard(data.currentCardNumber);
+});
+
 function showCard(n) {
   if (n === 1 || n === 6) {
     hideFooter();
@@ -6,11 +53,41 @@ function showCard(n) {
     showFooter();
     showHeader();
   }
-
+  data.currentCardNumber = n
   document
     .querySelectorAll("[data-card]")
     .forEach((x) => (x.style.display = "none"));
- document.querySelector(`[data-card='${n}']`).style.display = ""
+    const cardElement =  document.querySelector(`[data-card='${n}']`)
+    cardElement.style.display = "";
+
+    const nextButton = document.querySelector("[data-next]");
+    nextButton.disabled = true
+
+
+    if(n === 2){
+        cardElement.querySelectorAll('input').forEach(inElem => {
+            inElem.removeAttribute('checked')
+            if(inElem.value === data.question2){
+                inElem.setAttribute('checked',true)
+            }
+        })
+        if(data.question2){
+            nextButton.disabled = false
+        }
+    }
+
+
+    if(n === 4){
+        cardElement.querySelectorAll('input').forEach(inElem => {
+            inElem.removeAttribute('checked')
+            if(inElem.value === data.question4){
+                inElem.setAttribute('checked',true)
+            }
+        })
+        if(data.question4){
+            nextButton.disabled = false
+        }
+    }
 }
 
 function showFooter() {
